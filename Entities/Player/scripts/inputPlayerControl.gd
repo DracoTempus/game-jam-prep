ea5ebd0ce135b0ad07471b4d3437df93afa41e38
DracoTempus <D124c0@gmail.com>
2026-06-17 11:50:20 -0500

    art?
    Movement and art and camera

--- Entities/Player/scripts/inputPlayerControl.gd
+++ Entities/Player/scripts/inputPlayerControl.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-@export var speed := 350.0
+@export var speed := 100.0
 
 func _physics_process(_delta):
 	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
