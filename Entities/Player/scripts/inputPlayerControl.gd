fa4b903fa0f7afd88d11703de3e64309662c09c0
DracoTempus <D124c0@gmail.com>
2026-06-19 13:30:21 -0500

    pushing small stuff
    moving to other branch

--- Entities/Player/scripts/inputPlayerControl.gd
+++ Entities/Player/scripts/inputPlayerControl.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 @export var speed := 100.0
 
-func _physics_process(_delta):
-	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input * speed
+func _physics_process(delta):
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	if not is_on_floor():
+		velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
+	else:
+		velocity.y = 0
 	move_and_slide()
-
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
