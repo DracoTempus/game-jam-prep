eeccab7a75d6c34f9219f3ad32fdb89d4ab96b56
DracoTempus <D124c0@gmail.com>
2026-06-19 17:54:00 -0500

    Lets goooooo
    Some flying and animation

--- Entities/Player/scripts/inputPlayerControl.gd
+++ Entities/Player/scripts/inputPlayerControl.gd
@@ -1,28 +1,53 @@
 extends CharacterBody2D
 
+
 @export var ground_speed: float = 130.0
 @export var fly_speed: float = 200.0
 
 @export var fly_add_time: float = 5.0
 @export var fly_max_time: float = 8.0
-@export var fly_launch_velocity: float = 1600.0
+@export var launch_off_timer: float = 0.5
+@export var fly_launch_velocity: float = 600.0
+
+var is_launching: bool = false
+var launch_off_time: float = 0.5
+
+
+
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity") as float
 var fly_time_left: float = 0.0
 var is_flying: bool = false
 
+
 func _physics_process(delta: float) -> void:
+	if is_launching:
+		launch_off_timer -= delta
+		velocity.x = 0.0
+		velocity.y = -fly_launch_velocity
+		is_flying = true
+
+		if launch_off_timer <= 0.01:
+			is_launching = false
+
+		move_and_slide()
+
+		GlobalSignalsManager.fly_time_left = fly_time_left
+		GlobalSignalsManager.max_fly_time = fly_max_time
+		return
+	
 	var was_flying: bool = is_flying
 
 	if Input.is_action_just_pressed("fly"):
 		fly_time_left = min(fly_time_left + fly_add_time, fly_max_time)
 
-		if not was_flying:
-			velocity.y = -fly_launch_velocity
-
 	if fly_time_left > 0.0:
 		fly_time_left -= delta
 		is_flying = true
+		if not was_flying:
+			is_launching = true
+			launch_off_timer = launch_off_time
+			$AnimationPlayer.play("flying")
 	else:
 		fly_time_left = 0.0
 		is_flying = false
@@ -39,7 +64,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		var direction: float = Input.get_axis("move_left", "move_right")
 		velocity.x = direction * ground_speed
-
 		if not is_on_floor():
 			velocity.y += gravity * delta
 		else:
@@ -49,13 +73,13 @@ func _physics_process(delta: float) -> void:
 		$Sprite2D.flip_h = velocity.x > 0.0
 
 	move_and_slide()
+	GlobalSignalsManager.fly_time_left = fly_time_left
+	GlobalSignalsManager.max_fly_time = fly_max_time
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
 	pass
---
+++ Globals/GlobalSignalsManager/GlobalSignalsManager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var fly_time_left: float = 8.0
+var max_fly_time: float = 8.0
