91b9551c27ab3bf726eb1efee2eb93e540716d3a
DracoTempus <D124c0@gmail.com>
2026-06-20 15:13:07 -0500

    Globals
    and fixes

--- Entities/Player/scripts/inputPlayerControl.gd
+++ Entities/Player/scripts/inputPlayerControl.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 @export var ground_speed: float = 130.0
 @export var fly_speed: float = 230.0
 
@@ -19,7 +18,7 @@ var is_launching: bool = false
 var launch_off_timer: float = 0.1
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity") as float
-var fly_time_left: float = 0.0
+
 var is_flying: bool = false
 var is_attacking: bool = false
 
@@ -39,25 +38,23 @@ func _physics_process(delta: float) -> void:
 
 		move_and_slide()
 		if Input.is_action_just_pressed("fly"):
-				fly_time_left = min(fly_time_left + fly_add_time, fly_max_time)
-		GlobalSignalsManager.fly_time_left = fly_time_left
-		GlobalSignalsManager.max_fly_time = fly_max_time
+				GlobalSignalsManager.fly_time_left = min(GlobalSignalsManager.fly_time_left + GlobalSignalsManager.fly_add_time, GlobalSignalsManager.max_fly_time)
 		return
 	
 	var was_flying: bool = is_flying
 
 	if Input.is_action_just_pressed("fly"):
-		fly_time_left = min(fly_time_left + fly_add_time, fly_max_time)
+		GlobalSignalsManager.fly_time_left = min(GlobalSignalsManager.fly_time_left + GlobalSignalsManager.fly_add_time, GlobalSignalsManager.max_fly_time)
 
-	if fly_time_left > 0.0:
-		fly_time_left -= delta
+	if GlobalSignalsManager.fly_time_left > 0.0:
+		GlobalSignalsManager.fly_time_left -= delta
 		is_flying = true
 		
 		if not was_flying:
 			is_launching = true
 			launch_off_timer = launch_off_time
 	else:
-		fly_time_left = 0.0
+		GlobalSignalsManager.fly_time_left = 0.0
 		is_flying = false
 
 	if is_flying:
@@ -79,10 +76,9 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	toggle_damage_box(is_flying)
-	GlobalSignalsManager.fly_time_left = fly_time_left
-	GlobalSignalsManager.max_fly_time = fly_max_time
+	
 	if is_on_floor() and is_flying and not is_launching:
-		fly_time_left = 0.0
+		GlobalSignalsManager.fly_time_left = 0.0
 		is_flying = false
 
 	if is_launching:
--- Globals/GlobalSignalsManager/GlobalSignalsManager.gd
+++ Globals/GlobalSignalsManager/GlobalSignalsManager.gd
@@ -1,4 +1,7 @@
 extends Node
 
+#Character Stats
 var fly_time_left: float = 8.0
-var max_fly_time: float = 8.0
+var fly_add_time: float = 0.25
+var max_fly_time: float = 2.0
+var launch_off_time: float = 0.2
