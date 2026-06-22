a64cd329d573a09b30e2d61ab4c08dcd85193bb6
DracoTempus <D124c0@gmail.com>
2026-06-21 23:26:29 -0500

    casino starter
    starting casino

--- Entities/Player/scripts/CameraHitchcock.gd
+++ Entities/Player/scripts/CameraHitchcock.gd
@@ -2,15 +2,15 @@ extends Camera2D
 
 @export var follow_speed: float = 5.0
 
-@export var left_bound: Node2D
-@export var right_bound: Node2D
-@export var bottom_bound: Node2D
 @export var target: CharacterBody2D
+@export var current_bounds: Area2D
+
+var world_bounds: Rect2
 
 func _ready() -> void:
 	top_level = true
 	make_current()
-
+	set_area_bounds()
 	global_position = target.global_position
 
 
@@ -22,22 +22,31 @@ func _physics_process(delta: float) -> void:
 	var half_size: Vector2 = get_camera_half_size()
 
 	global_position = global_position.lerp(wanted_position, follow_speed * delta)
-	global_position = clamp_to_world_bounds(global_position, half_size)
+	global_position = clamp_to_bounds(global_position, half_size)
 
 
 func get_camera_half_size() -> Vector2:
 	var viewport_size: Vector2 = get_viewport_rect().size
 	return viewport_size * 0.5 * zoom
 
+func set_area_bounds() :
+	var collision_shape: CollisionShape2D = current_bounds.get_node("CollisionShape2D")
+	var rect_shape: RectangleShape2D = collision_shape.shape as RectangleShape2D
 
-func clamp_to_world_bounds(pos: Vector2, half_size: Vector2) -> Vector2:
-	if left_bound != null:
-		pos.x = max(pos.x, left_bound.global_position.x + half_size.x)
+	var size: Vector2 = rect_shape.size * collision_shape.global_scale.abs()
+	var top_left: Vector2 = collision_shape.global_position - size * 0.5
+	world_bounds = Rect2(top_left, size)
 
-	if right_bound != null:
-		pos.x = min(pos.x, right_bound.global_position.x - half_size.x)
+func clamp_to_bounds(pos: Vector2, half_size: Vector2) -> Vector2:
+	if current_bounds == null:
+		return pos
 
-	if bottom_bound != null:
-		pos.y = min(pos.y, bottom_bound.global_position.y - half_size.y)
+	var left: float = world_bounds.position.x
+	var right: float = world_bounds.position.x + world_bounds.size.x
+	var top: float = world_bounds.position.y
+	var bottom: float = world_bounds.position.y + world_bounds.size.y
+
+	pos.x = clampf(pos.x, left + half_size.x, right - half_size.x)
+	pos.y = clampf(pos.y, top + half_size.y, bottom - half_size.y)
 
 	return pos
--- Levels/scripts/CasinoDoors.gd
+++ Levels/scripts/CasinoDoors.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
 @export var target: Marker2D
+@export var camera: Camera2D
+@export var camera_bounds:Area2D
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") && GlobalSignalsManager.is_day_time:
 		body.global_position = target.global_position
+		camera.current_bounds = camera_bounds
+		camera.set_area_bounds()
+		camera.global_position = target.global_position
+		
