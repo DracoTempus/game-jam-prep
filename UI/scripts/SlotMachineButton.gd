1a028b1e6d91487a16b801631e40def7caa5361f
DracoTempus <D124c0@gmail.com>
2026-06-25 13:10:38 -0500

    Sounds and stuff for slot machine

--- UI/scripts/SlotMachineButton.gd
+++ UI/scripts/SlotMachineButton.gd
@@ -1,7 +1,11 @@
 extends Control
+@export var good_sound: AudioStream
+@export var bad_sound: AudioStream
+
+@onready var audio_player := AudioStreamPlayer.new()
 
 @export var spin_button: Button
-
+var is_spinning: bool = false
 @export var reel_1: VBoxContainer
 @export var reel_2: VBoxContainer
 @export var reel_3: VBoxContainer
@@ -13,7 +17,8 @@ var slot_grid: Array = []
 
 func _ready() -> void:
 	rng.randomize()
-
+	add_child(audio_player)
+	
 	if spin_button:
 		spin_button.pressed.connect(_on_spin_button_pressed)
 	else:
@@ -54,11 +59,11 @@ func _spawn_falling_reward(reel: Control, reward: SlotReward) -> void:
 
 
 func _on_spin_button_pressed() -> void:
-	print("Spin button pressed")
-
-	if rewards.is_empty():
-		print("No rewards assigned.")
+	if is_spinning:
 		return
+		
+	spin_button.disabled = true
+	is_spinning = true
 
 	var result: Array[SlotReward] = [
 		_get_random_reward_symbol(),
@@ -95,33 +100,11 @@ func _on_spin_button_pressed() -> void:
 	_animate_reel_drop(reel_2)
 	await get_tree().create_timer(0.1).timeout
 	_animate_reel_drop(reel_3)
-	await get_tree().create_timer(0.6).timeout
+	await get_tree().create_timer(1).timeout
 	await check_each_cell()
-
-func check_each_cell() -> void:
-	if slot_grid.size() < 3:
-		print("slot_grid is not ready")
-		return
-
-	for reel_index in 3:
-		for row_index in 3:
-			var cell = slot_grid[reel_index][row_index]
-			var reward_name: String = cell["name"]
-
-			match reward_name:
-				"Duck":
-					print("Duck found at reel ", reel_index, " row ", row_index)
-
-				"Penguin":
-					print("Penguin found at reel ", reel_index, " row ", row_index)
-
-				"Giraffe":
-					print("Giraffe found at reel ", reel_index, " row ", row_index)
-
-				_:
-					print("Unknown reward: ", reward_name)
-			await blink_cell(reel_index, row_index)
-
+	await get_tree().create_timer(1).timeout
+	is_spinning = false
+	spin_button.disabled = false
 
 func _animate_reel_drop(reel: Control) -> void:
 
@@ -175,14 +158,78 @@ func _add_rewards_in_reel(reel: VBoxContainer, reward_list: Array[SlotReward]) -
 		reel_results.append(cell)
 
 	return reel_results
-	
-func blink_cell(reel_index: int, row_index: int) -> void:
+
+
+#####CHECK CELLS FOR STUFF#########
+
+func check_each_cell() -> void:
+	for reel_index in 3:
+		for row_index in 3:
+			var cell = slot_grid[reel_index][row_index]
+			var reward_name: String = cell["name"]
+			var is_bad : bool = false
+			var reward_string: String = ""
+			match reward_name:
+				"Duck":
+					print("Duck found at reel ", reel_index, " row ", row_index)
+					reward_string = "Duck +10"
+
+				"Penguin":
+					print("Penguin found at reel ", reel_index, " row ", row_index)
+					reward_string = "Penguin +10"
+					
+				"Parrot":
+					print("Penguin found at reel ", reel_index, " row ", row_index)
+					reward_string = "Penguin +10"
+					
+				"Owl":
+					print("Penguin found at reel ", reel_index, " row ", row_index)
+					reward_string = "Penguin +10"
+					
+				"Chicken":
+					print("Penguin found at reel ", reel_index, " row ", row_index)
+					reward_string = "Penguin +10"
+
+				"Giraffe":
+					is_bad = true
+					reward_string = "Giraffe -10"
+					print("Giraffe found at reel ", reel_index, " row ", row_index)
+					
+				"Buffalo":
+					is_bad = true
+					reward_string = "Giraffe -10"
+					
+				"Hippo":
+					is_bad = true
+					reward_string = "Giraffe -10"
+					
+				"Sloth":
+					is_bad = true
+					reward_string = "Giraffe -10"
+				_:
+					print("Unknown reward: ", reward_name)
+					
+			create_floating_label(cell, reward_string, is_bad)
+			await blink_cell(reel_index, row_index, is_bad)
+
+
+func blink_cell(reel_index: int, row_index: int, is_bad: bool) -> void:
 	var cell = slot_grid[reel_index][row_index]
 	var texture_rect: TextureRect = cell["node"]
+	
+	if is_bad:
+		audio_player.stream = bad_sound
+	else:
+		audio_player.stream = good_sound
 
+	audio_player.play()
 	for i in 2:
-		texture_rect.modulate = Color.GREEN
-		await get_tree().create_timer(0.05).timeout
+		if is_bad:
+			texture_rect.modulate = Color.RED
+		else :
+			texture_rect.modulate = Color.GREEN
+			
+		await get_tree().create_timer(0.1).timeout
 
 		texture_rect.modulate = Color.WHITE
 		await get_tree().create_timer(0.05).timeout
@@ -202,3 +249,57 @@ func blink_row(row_index: int) -> void:
 			texture_rect.modulate = Color.WHITE
 
 		await get_tree().create_timer(0.1).timeout
+
+func blink_column(reel_index: int) -> void:
+	for i in 3:
+		for row_index in 3:
+			var cell = slot_grid[reel_index][row_index]
+			var texture_rect: TextureRect = cell["node"]
+			texture_rect.modulate = Color.RED
+
+		await get_tree().create_timer(0.1).timeout
+
+		for row_index in 3:
+			var cell = slot_grid[reel_index][row_index]
+			var texture_rect: TextureRect = cell["node"]
+			texture_rect.modulate = Color.WHITE
+
+		await get_tree().create_timer(0.1).timeout
+
+func create_floating_label(cell: Dictionary, text: String, is_bad: bool) -> void:
+	var texture_rect: TextureRect = cell["node"]
+
+	if texture_rect == null or not is_instance_valid(texture_rect):
+		return
+
+	var label := Label.new()
+	label.text = text
+	
+	if is_bad:
+		label.modulate = Color(1, 0, 0, 1)
+	else :
+		label.modulate = Color(0, 1, 0, 1)
+		
+	label.z_index = 100
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.add_theme_font_size_override("font_size", 28)
+
+	add_child(label)
+
+	label.global_position = texture_rect.global_position + Vector2(20, 20)
+
+	var duration := 1.0
+
+	var tween := create_tween()
+	tween.set_parallel(true)
+
+	tween.tween_property(label, "global_position", label.global_position + Vector2(0, -40), duration)\
+		.set_trans(Tween.TRANS_QUAD)\
+		.set_ease(Tween.EASE_OUT)
+
+	tween.tween_property(label, "modulate:a", 0.0, duration)
+
+	tween.finished.connect(func():
+		if is_instance_valid(label):
+			label.queue_free()
+	)
